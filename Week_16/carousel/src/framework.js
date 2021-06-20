07482bc0dd29aa314407563eaaa9183e5a14c831
吕嘉文 <daughlas@gmail.com>
2021-06-20 19:00:16 +0800

    chore: finish week 16

--- Week_16/carousel/src/framework.js
+++ Week_16/carousel/src/framework.js
@@ -1,61 +1,85 @@
 export function createElement(type, attributes, ...children) {
-  let el;
-  if (typeof type === "string") el = new ElementWrapper(type);
-  else el = new type();
-
-  for (let attrName in attributes) {
-    el.setAttribute(attrName, attributes[attrName]);
-  }
-
-  for (let child of children) {
-    if (typeof child === "string") {
-      child = new TextWrapper(child);
+    console.log('type: ', type);
+    let element;
+    if (typeof type === "string") {
+        element = new ElementWrapper(type);
+    } else {
+        element = new type();
     }
-    el.appendChild(child);
-  }
-  return el;
+
+    for (let attrName in attributes) {
+        element.setAttribute(attrName, attributes[attrName]);
+    }
+
+    let processChildren = (children) => {
+        for (let child of children) {
+            if (typeof child === 'object' && (child instanceof Array)) {
+                processChildren(child);
+                continue;
+            }
+            if (typeof child === "string") {
+                child = new TextWrapper(child);
+            }
+            element.appendChild(child);
+        }
+    }
+    processChildren(children);
+
+
+    return element;
 }
 
 export const STATE = Symbol('state');
 export const ATTRIBUTE = Symbol('attribute');
 
 export class Component {
-  constructor(type) {
-    this[ATTRIBUTE] = Object.create(null);
-    this[STATE] = Object.create(null);
-  }
-
-  setAttribute(name, value) {
-    this[ATTRIBUTE][name] = value;
-  }
-
-  appendChild(child) {
-    child.mountTo(this.root);
-  }
-
-  mountTo(parent) {
-    if (!this.root) {
-      this.render();
+    constructor(type) {
+        this[ATTRIBUTE] = Object.create(null);
+        this[STATE] = Object.create(null);
     }
-    parent.appendChild(this.root);
-  }
 
-  triggerEvent(type, args) {
-    this[ATTRIBUTE]['on' + type.replace(/^[\s\S]/, s => s.toUpperCase())](new CustomEvent(type, {detail: args}));
-  }
+    render() {
+        return this.root;
+    }
+
+    setAttribute(name, value) {
+        this[ATTRIBUTE][name] = value;
+    }
+
+    appendChild(child) {
+        // TODO 这个地方为啥不一样
+        if (!this.root) {
+            this.render();
+        }
+        child.mountTo(this.root);
+    }
+
+    mountTo(parent) {
+        if (!this.root) {
+            this.render();
+        }
+        parent.appendChild(this.root);
+    }
+
+    triggerEvent(type, args) {
+        this[ATTRIBUTE]['on' + type.replace(/^[\s\S]/, s => s.toUpperCase())](new CustomEvent(type, {detail: args}));
+    }
 }
 
 
 class ElementWrapper extends Component {
-  constructor(type) {
-    super();
-    this.root = document.createElement(type);
-  }
+    constructor(type) {
+        super();
+        this.root = document.createElement(type);
+    }
+    setAttribute(name, value) {
+        this.root.setAttribute(name, value);
+    }
 }
 
 class TextWrapper extends Component {
-  constructor(content) {
-    super();
-    this.root = document.createTextNode(content);
-  }
+    constructor(content) {
+        super();
+        this.root = document.createTextNode(content);
+    }
 }
